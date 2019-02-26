ff5e29f55feb11e404dc40e6b6ca5c37494bf1ab
Evgeny Kolokolov <8428631@gmail.com>
2019-02-26 14:37:27 +0300

    add AccountServiceImplTest

--- src/test/java/ea/kolokolov/service/AccountServiceImplTest.java
+++ src/test/java/ea/kolokolov/service/AccountServiceImplTest.java
@@ -5,21 +5,25 @@ import ea.kolokolov.model.Account;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import static java.math.RoundingMode.HALF_UP;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
 
 public class AccountServiceImplTest {
 
     private static final BigDecimal BALANCE = new BigDecimal(6642436).setScale(2, HALF_UP);
     private static final int ACCOUNT_NUMBER = 249869986;
     private static final int USER_ID = 2;
+    private static final String LOGIN = "iivanov";
 
     @Mock
     private AccountDao accountDao;
@@ -41,19 +45,33 @@ public class AccountServiceImplTest {
         //then
         Account actualAccount = service.getAccount(ACCOUNT_NUMBER);
         verify(accountDao).getAccount(ACCOUNT_NUMBER);
-        Assert.assertEquals(actualAccount, account);
+        assertEquals(actualAccount, account);
 
     }
 
     @Test
     public void testGetAccounts() {
+        //given
+        List<Account> accounts = new ArrayList<>();
+        accounts.add(new Account(USER_ID, ACCOUNT_NUMBER, BALANCE));
+        //when
+        when(accountDao.getAccounts(LOGIN)).thenReturn(accounts);
+        //then
+        List<Account> actualAccounts = service.getAccounts(LOGIN);
+        verify(accountDao).getAccounts(LOGIN);
+        assertFalse(actualAccounts.isEmpty());
+        assertEquals(actualAccounts, accounts);
     }
 
-    @Test
+    @Test(expectedExceptions = UnsupportedOperationException.class)
     public void testCreateAccount() {
+        //then
+        service.createAccount(new Account());
     }
 
-    @Test
+    @Test(expectedExceptions = UnsupportedOperationException.class)
     public void testDeleteAccount() {
+        //then
+        service.deleteAccount(ACCOUNT_NUMBER);
     }
 }
